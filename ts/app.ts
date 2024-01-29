e5a8a72568fa879c30068800e1ae7f53255058a6
“Dev-almazan” <ernestoalmazanresendiz@gmail.com>
2024-01-29 12:34:51 -0600

    Agregando funciones a tarjets

--- ts/app.ts
+++ ts/app.ts
@@ -25,9 +25,9 @@ function ASignarUsuarios(data :usuario):void
     card.innerHTML = `<div class="card p-4 shadow-sm" style="width: 100%;">
                                <img src="./img/${data.avatar}" class="card-img-top" alt="${data.profesion}" >     
                                 <div class="card-body">
-                                    <h5 class="card-title">${data.nombre} ${data.apellidos}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">${data.profesion} </h6>
-                                    <p class="card-text"><b>Cursos aprobados:</b>${cursosAprobados}</p>
+                                    <h5 class="card-title ">${data.nombre} ${data.apellidos}</h5>
+                                    <h6 class="card-subtitle  mb-2 text-muted">${data.profesion} </h6>
+                                    <p class="card-text "><b>Cursos con certificado: </b>${cursosAprobados}</p>
                                     <a href="#" class="card-link btn-primary btn">Linkedin</a>
                                     <a href="#" class="card-link btn-dark btn">Git Hub</a>
                                 </div>
